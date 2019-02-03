04e3ff076767788579a9a8d4a4d6d62a6c79bbf9
neliza <nicolasaelf64@gmail.com>
2019-02-03 14:33:18 -0700

    reset audio currentTime on keyDown

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.onload = function(){
 
-    //We want to listen for the key being pressed
+//We want to listen for the key being pressed
 window.addEventListener('keydown', function(e) {
     //console.log(e.keyCode);
 
@@ -8,6 +8,11 @@ window.addEventListener('keydown', function(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
    
     if(!audio) return; //stop function from running altogether if there is no audio component
+
+    //We want to rewind the audio element to the beginning so it can be played in succession
+    //The currentTime property sets or returns the current position (in seconds) of the audio/video playback.
+    //When setting this property, the playback will jump to the specified position. In this case 0 (the start)
+    audio.currentTime = 0;
 
     //play audio element
     audio.play();
